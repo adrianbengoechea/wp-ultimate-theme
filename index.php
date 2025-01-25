4d871eb0b2e8d725b22b430e1e5c31865b44afe4
Adrian Bengoechea <ebel.bengoechea@gmail.com>
2025-01-25 02:28:09 -0600

    fetch posts & increment counter button using vuejs

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 
 <?php get_header(); ?>
 
-<main>
-<h1>{{title}}</h1>
+<main role="main">
+    <div class="container">
+        <div class="container--wrap">
+            <h1>{{title}}</h1>
 
-<?php echo site_url(); ?>
+
+                    <div class="py-4">
+                        <button class="js--show-posts btn btn-primary me-2" @click="loadPosts()">Show Posts</button>
+                        <button class="btn btn-primary" @click="count++">Count ({{count}})</button>
+                    </div>
+                    <div class="py-4 js--show-posts-response">
+                        <ul>
+                            <li v-for="post in trackedPosts">
+                                <a :href="post.link" target="_blank"><strong>{{ post.title.rendered }}</strong></a>    
+                            </li>
+                        </ul>
+                    <div>
+                
+
+            <?php echo site_url(); ?>
+        </div>
+    </div>
 </main>
 
 <?php get_footer(); ?>
--- resources/js/main.js
+++ resources/js/main.js
@@ -6,8 +6,34 @@
     createApp({
       setup() {
         const title = ref('Hello vue!')
+        const count = ref(0);
+
+        const endpoint = '/wp-json';
+        const postsEndpoint = endpoint + '/wp/v2/posts';
+
+        const trackedPosts = ref([]);
+
+        async function loadPosts () {
+
+          console.log('load posts');
+
+          try {
+            const response = await fetch(postsEndpoint);
+            if (!response.ok) {
+              throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            console.log(json);
+
+            this.trackedPosts = json;
+          } catch (error) {
+            console.error(error.message);
+          }
+
+        }
         return {
-          title
+          title, count, loadPosts, trackedPosts
         }
       }
     }).mount( siteAppID )
